6ab42db563e2c6176fb67a159a86086f8d2c53cc
hesit8 <45435916+Greg311@users.noreply.github.com>
2020-06-24 16:51:50 -0700

    added SPI pins

--- Marlin/src/pins/lpc1769/pins_TINYFABV5.h
+++ Marlin/src/pins/lpc1769/pins_TINYFABV5.h
@@ -39,17 +39,59 @@
 // Servos
 //
 #define SERVO0_PIN         P2_13
-#define SERVO1_PIN         P1_19
+
+//
+// TMC StallGuard DIAG pins
+//
+#define X_DIAG_PIN                         P1_24  // X-STOP
+#define Y_DIAG_PIN                         P1_26  // Y-STOP
+#define Z_DIAG_PIN                         P1_28  // Z-STOP
+//#define E0_DIAG_PIN                        P1_26  // E0DET
 
 //
 // Limit Switches
 //
-#define X_MIN_PIN          P1_24
+//#define X_MIN_PIN          P1_24
 //#define X_MAX_PIN          P1_24
-#define Y_MIN_PIN          P1_26
+//#define Y_MIN_PIN          P1_26
 //#define Y_MAX_PIN          P1_26
-#define Z_MIN_PIN          P1_28
+//#define Z_MIN_PIN          P1_28
 //#define Z_MAX_PIN          P1_28
+
+#ifdef X_STALL_SENSITIVITY
+  #define X_STOP_PIN                  X_DIAG_PIN
+  #if X_HOME_DIR < 0
+    #define X_MAX_PIN                      P1_24  // E0DET
+  #else
+    #define X_MIN_PIN                      P1_24  // E0DET
+  #endif
+#else
+  #define X_STOP_PIN                       P1_24  // X-STOP
+#endif
+
+#ifdef Y_STALL_SENSITIVITY
+  #define Y_STOP_PIN                  Y_DIAG_PIN
+  #if Y_HOME_DIR < 0
+    #define Y_MAX_PIN                      P1_26  // E1DET
+  #else
+    #define Y_MIN_PIN                      P1_26  // E1DET
+  #endif
+#else
+  #define Y_STOP_PIN                       P1_26  // Y-STOP
+#endif
+
+#ifdef Z_STALL_SENSITIVITY
+  #define Z_STOP_PIN                  Z_DIAG_PIN
+  #if Z_HOME_DIR < 0
+    #define Z_MAX_PIN                      P1_28  // PWRDET
+  #else
+    #define Z_MIN_PIN                      P1_28  // PWRDET
+  #endif
+#else
+  #ifndef Z_STOP_PIN
+    #define Z_STOP_PIN                     P1_28  // Z-STOP
+  #endif
+#endif
 
 //
 // Steppers
@@ -57,22 +99,85 @@
 #define X_STEP_PIN         P2_00
 #define X_DIR_PIN          P0_05
 #define X_ENABLE_PIN       P2_04
+#ifndef X_CS_PIN
+  #define X_CS_PIN         P1_20
+#endif
 
 #define Y_STEP_PIN         P2_01
 #define Y_DIR_PIN          P0_11
 #define Y_ENABLE_PIN       P2_04
+#ifndef Y_CS_PIN
+  #define Y_CS_PIN         P0_00
+#endif
 
 #define Z_STEP_PIN         P2_02
 #define Z_DIR_PIN          P0_20
 #define Z_ENABLE_PIN       P2_04
+#ifndef Z_CS_PIN
+  #define Z_CS_PIN         P0_01
+#endif
 
 #define E0_STEP_PIN        P2_03
 #define E0_DIR_PIN         P0_22
 #define E0_ENABLE_PIN      P2_04
+#ifndef E0_CS_PIN
+  #define E0_CS_PIN        P1_21
+#endif
 
-#define E1_STEP_PIN        P2_08
-#define E1_DIR_PIN         P2_13
-#define E1_ENABLE_PIN      P2_04
+
+//
+// Software SPI pins for TMC2130 stepper drivers
+//
+#if ENABLED(TMC_USE_SW_SPI)
+  #ifndef TMC_SW_MOSI
+    #define TMC_SW_MOSI                    P0_09
+  #endif
+  #ifndef TMC_SW_MISO
+    #define TMC_SW_MISO                    P0_08
+  #endif
+  #ifndef TMC_SW_SCK
+    #define TMC_SW_SCK                     P0_07
+  #endif
+#endif
+
+#if HAS_TMC_UART
+  /**
+   * TMC2208/TMC2209 stepper drivers
+   *
+   * Hardware serial communication ports.
+   * If undefined software serial is used according to the pins below
+   */
+  //#define X_HARDWARE_SERIAL  Serial
+  //#define X2_HARDWARE_SERIAL Serial1
+  //#define Y_HARDWARE_SERIAL  Serial1
+  //#define Y2_HARDWARE_SERIAL Serial1
+  //#define Z_HARDWARE_SERIAL  Serial1
+  //#define Z2_HARDWARE_SERIAL Serial1
+  //#define E0_HARDWARE_SERIAL Serial1
+  //#define E1_HARDWARE_SERIAL Serial1
+  //#define E2_HARDWARE_SERIAL Serial1
+  //#define E3_HARDWARE_SERIAL Serial1
+  //#define E4_HARDWARE_SERIAL Serial1
+
+  //
+  // Software serial
+  //
+  #define X_SERIAL_TX_PIN                  P1_25
+  #define X_SERIAL_RX_PIN                  P1_25
+
+  #define Y_SERIAL_TX_PIN                  P2_12
+  #define Y_SERIAL_RX_PIN                  P2_12
+
+  #define Z_SERIAL_TX_PIN                  P2_13
+  #define Z_SERIAL_RX_PIN                  P2_13
+
+  #define E0_SERIAL_TX_PIN                 P1_30
+  #define E0_SERIAL_RX_PIN                 P1_30
+
+
+  // Reduce baud rate to improve software serial reliability
+  #define TMC_BAUD_RATE 19200
+#endif
 
 //
 // Temperature Sensors
