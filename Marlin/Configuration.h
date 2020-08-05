36fcbce0fd61f984ce360b1d8d6f1c79f7a66932
hesit8 <45435916+Greg311@users.noreply.github.com>
2020-08-05 12:26:56 -0700

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1008,7 +1008,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, -33, -1.30 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, -33, -0.50 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
